061f9aa4dca1763fd2ae94ce7b260dc789c4fade
kayanhales <kayanhales@Kayans-iMac.local>
2015-06-08 23:17:23 -0400

    Initial commit

---
+++ app/src/main/java/com/praisenation/onomo30/User.java
@@ -0,0 +1,17 @@
+package com.praisenation.onomo30;
+
+/**
+ * Created by kayanhales on 6/5/15.
+ */
+public class User {
+    public long userId;
+    public String username;
+    public String password;
+
+    public User(long userId, String username, String password){
+        this.userId=userId;
+        this.username=username;
+        this.password=password;
+    }
+
+}
